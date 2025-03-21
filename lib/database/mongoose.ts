33e25bce2813898e19193243b6c2a8ed4a5ca880
beledu <beleduedwin@yahoo.fr>
2025-03-21 17:26:22 +0100

    initial commit

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,32 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose
+
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
+}
+
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn; //vérifie si on a un cache de connexion, si c'est le cas on sort du proccess
+
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imagenea', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
